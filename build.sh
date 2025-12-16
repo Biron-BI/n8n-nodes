dc9cdde69f6f54c5190e9014b9501a5b0bbe5bf0
Antoine <antoine.lempereur.dev@gmail.com>
2025-12-16 10:56:14 +0100

    build script

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 TAG=$(git describe --tags --abbrev=0)
 
+echo VERSION=$TAG
+
+npm run build
+
 docker build -f docker/Dockerfile -t europe-docker.pkg.dev/artifact-registry-412407/docker-image/n8n:$TAG .
 
 docker push europe-docker.pkg.dev/artifact-registry-412407/docker-image/n8n:$TAG
